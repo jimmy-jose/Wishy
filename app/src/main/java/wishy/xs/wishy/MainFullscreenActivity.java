a4640c57225219d8521b77eb5b2e2abd9b1b928e
Jimmy <jimmy.j@zemosolabs.com>
2017-07-27 14:49:09 +0530

    Added Full Screen Activity

--- app/src/main/java/wishy/xs/wishy/MainFullscreenActivity.java
+++ app/src/main/java/wishy/xs/wishy/MainFullscreenActivity.java
@@ -2,9 +2,17 @@ package wishy.xs.wishy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainFullscreenActivity extends Activity {
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,17 @@ public class MainFullscreenActivity extends Activity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_main_fullscreen);
+
+//        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+//        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
+
+
 }
