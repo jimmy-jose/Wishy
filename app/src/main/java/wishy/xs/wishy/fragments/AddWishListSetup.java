dc85ef9f3decb53b9099acafc2c6fb3e2731b3c4
Jijo <jimmy.jose96@gmail.com>
2017-11-25 12:52:51 +0530

    Icons And Splash Icon Add Wish List Item Theme change

---
+++ app/src/main/java/wishy/xs/wishy/fragments/AddWishListSetup.java
@@ -0,0 +1,29 @@
+package wishy.xs.wishy.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jimmy on 8/6/2017.
+ */
+
+public class AddWishListSetup extends Fragment {
+    private Context mContext;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
